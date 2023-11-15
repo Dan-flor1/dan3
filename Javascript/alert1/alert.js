98af3031418d464b18cce3a13dec8e97a3b59595
Dan-flor1 <143449166+Dan-flor1@users.noreply.github.com>
2023-11-15 21:02:47 +0000

    r

--- Javascript/alert1/alert.js
+++ Javascript/alert1/alert.js
@@ -4,5 +4,7 @@ for(let i=1; i<=100;i++)
 {
     sum=sum+i;
 }
-alert("Answer = " + sum);
-console.log("Answer = " + sum);
+//alert("Answer = " + sum);
+//console.log("Answer = " + sum);
+let answer = document.querySelector('p'); //Answer will refer to the p tag in hte HTML document
+answer.innerHTML = "Answer = " + sum;//change the innerHTML of the <p>
