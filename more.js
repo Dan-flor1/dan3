1a005bed4c5251a6385bced02df4eaa9377982d9
Dan-flor1 <143449166+Dan-flor1@users.noreply.github.com>
2023-12-18 20:57:51 +0000

    dsdf

--- more.js
+++ more.js
@@ -1,5 +1,25 @@
 let fname = document.getElementById("fname");
 let lname= document.getElementById("lname");
 
-fname.value = "Thomas";
-alert(fname.value);
+//fname.value = "Thomas";
+//alert(fname.value);
+
+
+//get access to radio buttons
+let radio_html= document.getElementById("html");
+let radio_css= document.getElementById("css");
+let radio_js = document.getElementById("javascript");
+
+//radio_html.checked=false;
+
+ //   alert(radio_html.checked);
+
+ let check_idk = document.getElementById("checkbox_idk");
+ let check_vocational = document.getElementById("checkbox_vocational");
+ let check_college = document.getElementById("checkbox_college");
+ let check_military = document.getElementById("checkbox_military");
+
+ check_idk.check=true;
+ {
+    alert(check_idk.checked);
+ }
